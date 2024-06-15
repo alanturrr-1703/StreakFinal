295f10d4b1398726f50544fa1d94597b0988d1d7
alanturrr-1703 <hariom.nthakkar@gmail.com>
2024-06-15 16:22:20 +0530

    nice

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const userStreakRouter = require('./routes/userStreak');
 const usersRouter = require('./routes/users');
 
 // Middleware
