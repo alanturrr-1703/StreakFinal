d664677ed16d3a315658138bc6e76bd09eca25e2
alanturrr-1703 <101917244+alanturrr-1703@users.noreply.github.com>
2024-06-18 08:54:36 +0000

    Entry point made

---
+++ index.js
@@ -0,0 +1,9 @@
+const { summary } = require('./features/summary');
+const { streakRanges } = require('./features/streakRanges');
+const { trackRecord } = require('./features/trackRecord');
+
+module.exports = {
+  summary,
+  streakRanges,
+  trackRecord
+};
