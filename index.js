17844c454bb3784114750d3570ed11ba226d545c
alanturrr-1703 <hariom.nthakkar@gmail.com>
2024-06-21 13:40:02 +0530

    Nice ig resolved

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { Summary } from './features/summary.js';
 import { streakRanges } from './features/streakRanges.js';
 import { trackRecord } from './features/trackRecord.js';
 module.exports = {
-  summary,
+  Summary,
   streakRanges,
   trackRecord
 };
