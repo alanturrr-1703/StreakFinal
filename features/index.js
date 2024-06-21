725ef71a5d215b17d002a17833322e0ba96b11aa
Harshit Thakkar <101917244+alanturrr-1703@users.noreply.github.com>
2024-06-21 11:50:22 +0530

    Whatever

--- features/index.js
+++
@@ -1,9 +0,0 @@
-const summary = require('./features/summary');
-const streakRanges = require('./features/streakRanges');
-const trackRecord = require('./features/trackRecord');
-
-module.exports = {
-  summary,
-  streakRanges,
-  trackRecord,
-};
